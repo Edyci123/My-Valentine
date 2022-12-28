faf1977c841e118b53edbd6eeb4807942e7c255a
Edi Bicu <bicuedici@gmail.com>
2022-12-28 12:16:56 +0200

    Added AOS

--- frontend/src/app/components/reasons/reasons.component.ts
+++ frontend/src/app/components/reasons/reasons.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as AOS from 'aos';
+
 @Component({
   selector: 'app-reasons',
   templateUrl: './reasons.component.html',
-  styleUrls: ['./reasons.component.scss']
+  styleUrls: ['./reasons.component.scss'],
 })
 export class ReasonsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    AOS.init()
   }
-
 }
